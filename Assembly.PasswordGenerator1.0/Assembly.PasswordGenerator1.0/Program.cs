bab5ccfd69858b82d7a10aca78194e648f1179d8
Pedro Bastos <139172281+PerdoBastos@users.noreply.github.com>
2023-12-03 18:22:48 +0000

    Add files via upload

---
+++ Assembly.PasswordGenerator1.0/Assembly.PasswordGenerator1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Password generator");
+
+        int passwordlenght = 8;
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=";
+
+        Random random = new Random();
+        char[] pass = new char[passwordlenght];
+
+        for (int i = 0; i < passwordlenght; i++)
+        {
+            pass[i] = chars[random.Next(chars.Length)];
+        }
+
+        Console.WriteLine(pass);
+    }
+}
